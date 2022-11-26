bf966e1d38d88e7539f468b5ab6ce1a4a0781277
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2022-11-26 23:37:44 +0100

    FeedPage responsive

--- fitness-time-frontend/src/pages/feed.tsx
+++ fitness-time-frontend/src/pages/feed.tsx
@@ -1,4 +1,5 @@
 import { SimpleGrid, Stack } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import React, { useState } from "react";
 import { EventCard } from "../components/event/EventCard";
 import { FilterEventsComponent } from "../components/event/FilterEventsComponent";
@@ -8,6 +9,10 @@ import EventService from "../services/EventService";
 
 export default function FeedPage() {
   const [filteredList, setFilteredList] = useState<EventDto[]>([]);
+
+  const medium = useMediaQuery("(min-width: 600px)");
+  const large = useMediaQuery("(min-width: 950px)");
+  const extraLarge = useMediaQuery("(min-width: 1300px)");
 
   const eventService = EventService();
   const eventsQuery = eventService.useGetFeed();
@@ -20,7 +25,7 @@ export default function FeedPage() {
         filterKey="FeedPageFilter"
       />
       <QueryComponent resourceName="Feed" query={eventsQuery}>
-        <SimpleGrid cols={3}>
+        <SimpleGrid cols={extraLarge ? 4 : large ? 3 : medium ? 2 : 1}>
           {filteredList.map((event) => (
             <EventCard event={event} key={event.id} />
           ))}
